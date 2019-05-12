4335c83da75bacd06bf04923eeb501cb679f7a0e
Lira <liramail96@gmail.com>
2019-05-12 18:46:32 +0300

    start server with docker

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'myBlog'),
+            'database' => env('DB_DATABASE', 'myblog'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
